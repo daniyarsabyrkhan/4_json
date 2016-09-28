0ba35b922648fb34b057ef6441f433258d48add6
daniyar <daniyar.sabyrkhan@gmail.com>
2016-09-29 00:23:12 +0400

    проверьте, пожалуйста

--- pprint_json.py
+++ pprint_json.py
@@ -2,16 +2,17 @@ import json
 
 
 def load_data(filepath):
-    with open (filepath, "r") as jsondata:
-    	jsondatas = json.load(jsondata)
-    	return jsondatas
+    with open(filepath, "r") as jsonfile:
+        jsonlist = json.load(jsonfile)
+        return jsonlist
 
 
 def pretty_print_json(data):
-    print (json.dumps(data, indent = 4, sort_keys = True))
+    print ("Теперь удобно читать: \n %s" % json.dumps(
+        data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    filepath = input()
-    jsonfile = load_data(filepath)
-    pretty_print_json(jsonfile)
+    filepath = input("Укажите путь до json файла: ")
+    jsonlist = load_data(filepath)
+    pretty_print_json(jsonlist)

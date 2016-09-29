a8a3b9f671328ff24c0d10f10c07688c4ec2e049
daniyar <daniyar.sabyrkhan@gmail.com>
2016-09-29 17:12:46 +0400

    исправил замечание

--- pprint_json.py
+++ pprint_json.py
@@ -8,11 +8,10 @@ def load_data(filepath):
 
 
 def pretty_print_json(data):
-    print ("Теперь удобно читать: \n %s" % json.dumps(
-        data, indent=4, sort_keys=True))
+    return json.dumps(data, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
     filepath = input("Укажите путь до json файла: ")
     jsonlist = load_data(filepath)
-    pretty_print_json(jsonlist)
+    print ("Теперь удобно читать: \n %s" % pretty_print_json(jsonlist))
